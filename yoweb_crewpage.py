e8637c96d1acd9638d25428d9c86d115002354c6
BenZ-PC\Ben Zhao <ben.zhao159@gmail.com>
2018-08-01 02:34:49 +1000

    separation of recorder and crew crawl function

--- yoweb_crewpage.py
+++ yoweb_crewpage.py
@@ -17,9 +17,6 @@ TODO: - loop over specified time delay
 TODO: - plotting of statistics over time.
 """
 import pandas as pd
-from datetime import datetime
-import time
-import csv
 
 def retrieve_crew_info(crew_id):
     url = (
@@ -64,39 +61,4 @@ if __name__ == '__main__':
     # crew_id = 5001832  # Consider it Drunk
     # crew_id = 5000435  # The Southsea Bandits
     # crew_id = 5001990  # The Organization
-    n = 0
-    while True:
-
-        now_time = datetime.utcnow().strftime('%d/%m/%y-%H:%M:%S')
-
-        with open("bandits_job.csv", "ab") as crew_file:
-            writer = csv.writer(crew_file, delimiter=',', quotechar="\"",
-                                quoting=csv.QUOTE_NONNUMERIC)
-            job_list, tot_num = retrieve_crew_info(5000435)
-            line = [now_time, n]
-            line.append(tot_num.get("Jobbing Pirate", 0))
-            line.extend(job_list.get("Jobbing Pirate", ""))
-            writer.writerow(line)
-
-        with open("CIS_job.csv", "ab") as crew_file:
-            writer = csv.writer(crew_file, delimiter=',', quotechar="\"",
-                                quoting=csv.QUOTE_NONNUMERIC)
-            job_list, tot_num = retrieve_crew_info(5001832)
-            line = [now_time, n]
-            line.append(tot_num.get("Jobbing Pirate", 0))
-            line.extend(job_list.get("Jobbing Pirate", ""))
-            writer.writerow(line)
-
-        with open("GW_job.csv", "ab") as crew_file:
-            writer = csv.writer(crew_file, delimiter=',', quotechar="\"",
-                                quoting=csv.QUOTE_NONNUMERIC)
-            job_list, tot_num = retrieve_crew_info(5001990)
-            line = [now_time, n]
-            line.append(tot_num.get("Jobbing Pirate", 0))
-            line.extend(job_list.get("Jobbing Pirate", ""))
-            writer.writerow(line)
-
-        sleeptime = 60 - datetime.utcnow().second
-        time.sleep(sleeptime)
-
-        n += 1
+    print retrieve_crew_info(5000435)
