be2f3923d1adfa91b670408baf988126e1aaf3f4
BenZ-PC\Ben Zhao <ben.zhao159@gmail.com>
2018-08-05 22:37:42 +1000

    Modification of Crawler script to utilise yoweb crawler.
    Branch Delayed due to Yoweb issue of off by one rank member lists (does not parse the last box, commonly jobbers or cabin person)

---
+++ yoweb/helpers.py
@@ -0,0 +1,16 @@
+BASIC_ATTRS = ('rank', 'name')
+SHARES = ('jobbing_pirate', 'cabin_person', 'pirate', 'officer', 'fleet_officer', 'senior_officer', 'captain')
+RANKS = ['Captain', 'Senior Officer', 'Fleet Officer', 'Officer', 'Pirate', 'Cabin Person', 'Jobbing Pirate']
+
+def clean_stat(data):
+    stats = data.split('  ')
+    left_side = stats[0].split('/')
+    experience = left_side[0]
+    standing = {'ocean_wide': left_side[1]}
+    if len(stats) == 2:
+        right_side = stats[1].replace('(archipelago:\xa0', '').replace(')', '')
+        standing['archipelago'] = right_side
+    else:
+        standing['archipelago'] = left_side[1]
+
+    return experience, standing
